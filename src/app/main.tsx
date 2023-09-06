01c48bd05fb00acbe123b85fe1536cac8865e044
Alexander Bykovskiy <aleksandr.bykovskiy@sksoldev.com>
2023-09-06 19:13:25 +0400

    Cleared app and installed tailwind

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import GamePage from '../pages/game-page.tsx'
+import '../shared/styles/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GamePage />
   </React.StrictMode>,
 )
---
+++ src/pages/game-page.tsx
@@ -0,0 +1,15 @@
+function GamePage() {
+
+  return (
+    <div className="p-8 min-h-[100vh] bg-white flex flex-col justify-center gap-8">
+        <div className="flex justify-center">
+            the board here
+        </div>
+        <div className="flex justify-center">
+            the game history here
+        </div>
+    </div>
+  )
+}
+
+export default GamePage
