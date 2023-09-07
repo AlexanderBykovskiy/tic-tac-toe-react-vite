789ae8b6b892a9202465ac57054451976cfbac36
Alexander Bykovskiy <aleksandr.bykovskiy@sksoldev.com>
2023-09-07 19:49:14 +0400

    Added game board labels

--- src/features/game-board/ui/GameBoard.tsx
+++ src/features/game-board/ui/GameBoard.tsx
@@ -5,13 +5,32 @@ import GameField from "./GameField.tsx";
 
 const GameBoard: React.FC<typeGameBoardProps> = ({board}) => {
     return(
-        <div className="grid grid-cols-3">
-            {board.map((item, index) => {
-                return(
-                    <GameField key={index} fieldValue={item}/>
-                )
-            })}
+        <div className="relative">
+            <div className="absolute l-0 h-full w-0">
+                <div className="absolute h-full right-3 grid grid-rows-3 items-center text-gray-400 font-semibold">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
+            </div>
+
+            <div className="absolute top-0 h-0 w-full">
+                <div className="absolute bottom-2 w-full grid grid-cols-3 justify-items-center text-gray-400 font-semibold">
+                    <div>A</div>
+                    <div>B</div>
+                    <div>C</div>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-3">
+                {board.map((item, index) => {
+                    return(
+                        <GameField key={index} fieldValue={item}/>
+                    )
+                })}
+            </div>
         </div>
+
     )
 }
 
